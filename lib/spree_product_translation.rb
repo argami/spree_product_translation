2cceea7f9d2215f150c9057734b405fe5855f41c
Gamaliel A. Toro Herrera <argami@gmail.com>
2011-12-07 12:24:37 +0100

    - Eliminated version.rb just need Versionfile

--- lib/spree_product_translation.rb
+++ lib/spree_product_translation.rb
@@ -1,3 +1,2 @@
 require 'spree_core'
-require 'spree_product_translation/version'
 require 'spree_product_translation/engine'
--- lib/spree_product_translation/version.rb
+++
@@ -1,3 +0,0 @@
-module SpreeProductTranslation
-  VERSION = "0.0.1"
-end
