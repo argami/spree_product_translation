cf73ca53affe39dc1d642e98872a471ad5116931
Gamaliel A. Toro Herrera <argami@gmail.com>
2011-12-24 16:01:29 +0100

    Added a change to avoid a error

--- app/overrides/product_translation_js.rb
+++ app/overrides/product_translation_js.rb
@@ -2,4 +2,7 @@ Deface::Override.new(
   :virtual_path => "layouts/admin",
   :name => 'product_translation_js',
   :insert_bottom => "[data-hook='admin_inside_head']",
-  :text => %Q[<%= javascript_tag "$(function(){ $('.tabs').livequery(function() { $(this).tabs(); }); });" %>])
+  :text => %Q[<%= javascript_tag "$(function(){ 
+                      var tabs = $('.tabs');
+                      if (tabs.hasOwnProperty('livequery'))
+                        tabs.livequery(function() { $(this).tabs(); }); });" %>])
